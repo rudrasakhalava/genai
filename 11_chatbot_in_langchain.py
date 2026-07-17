15fb3eb224847c95cc46bb5ff0c2a9578c9d5f7f
Rudra Sakhalva <157484680+rudrasakhalava@users.noreply.github.com>
2026-07-17 11:45:42 +0530

    Improve the code of chatbot as per langchain.

--- 11_chatbot_in_langchain.py
+++ 11_chatbot_in_langchain.py
@@ -1,22 +1,25 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-5-nano")
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant.")
+]
 
 while True:
     user = input("YOU :: ")
-    chat_history.append(user)
+    chat_history.append(HumanMessage(content=user))
 
     if user == "exit" :
-        chat_history.append("Closed")
+        chat_history.append(AIMessage(content="Closed"))
         break
 
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI :: ",result.content)
 
 print(chat_history)
